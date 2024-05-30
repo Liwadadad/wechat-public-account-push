2dbd06ad8da651d6366c17a2bc3c6cf0e571235b
Liwadadad <15634550223@163.com>
2024-05-30 21:14:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,18 +8,18 @@
 const USER_CONFIG = {
 USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxfb15844b42ebce61',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '济南',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小静',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU30297TCnX6j5UZm7WoUYfm756XUrTDD7n2RoIQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,20 +29,20 @@ USE_PASSAGE: 'push-deer',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小静', year: '2005', date: '02-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小静', year: '2005', date: '02-06',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2024', date: '04-20',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2024-04-20' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -55,9 +55,9 @@ USE_PASSAGE: 'push-deer',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '旺崽崽',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
     }
   ],
 
