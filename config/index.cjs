b4e9dfe312f9acc40891f7505fe2cb64da1814b8
Liwadadad <15634550223@163.com>
2024-05-30 20:42:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ USE_PASSAGE: 'push-deer',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU30297TCnX6j5UZm7WoUYfm756XUrTDD7n2RoIQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '00001',
+      useTemplateId: 'bIORB6SXTcK7MmjH5iXACVqLAi5UZ2RPs6kXO7aFxl8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
