f08005bf8230daf1eb4b60a548a2e16ad2d26864
Liwadadad <15634550223@163.com>
2024-05-31 18:43:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ USE_PASSAGE: 'wechat-test',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'SS3ngyemjIUpXo9fVQ238Fu27yiiMRmw7fN3btjDGA4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
