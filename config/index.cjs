0016225b698cd4155ff1c8246b9ab71b5932b77f
Liwadadad <15634550223@163.com>
2024-05-31 17:36:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
 
   CALLBACK_USERS: [
     {
