a8c034fc25ef1456c9f1757e649fe9306c4c0f6d
Liwadadad <15634550223@163.com>
2024-06-01 16:31:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,38 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'F3lqui1IK02jAK-lIxMdqeIlwI_E0RgmF1GqVbH-Z1s',
+      useTemplateId: 'XuvimIyd5-O9fqf28yiSL5nGTUa6FfZDvnUPS9wWjP4',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-28',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小静', year: '2005', date: '12-28',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小静', year: '2005', date: '02-06',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2024', date: '04-20',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2024-04-20' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+        
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '小静',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'XuvimIyd5-O9fqf28yiSL5nGTUa6FfZDvnUPS9wWjP4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-28',
       festivals: [
