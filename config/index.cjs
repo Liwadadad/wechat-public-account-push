06aed7006515f15655c9ebbe4e6a05da9c9862e6
Liwadadad <15634550223@163.com>
2024-06-01 15:04:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'QB8Ti9EVilbOjLyhUWMNyT3_KkKvyEcBxZGed8NVKGQ',
+      useTemplateId: 'vOQVI0Sz2TS7DHzIGhrSf4dV5EfujRJALeltF7neqgQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-28',
       festivals: [
