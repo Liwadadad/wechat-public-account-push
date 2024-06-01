ecf6364541c2c578a6659b6a82e69c41dd218c32
Liwadadad <15634550223@163.com>
2024-06-01 16:51:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zI4_8_UDgLz4xQJ3inRMXq9XPYtwq_N4o9ybeNtkUwI',
+      useTemplateId: 'QUbJ2m28pLZuHFBOcmSiV9EtYWE_lubLJ-6JEOVdDmA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-28',
       festivals: [
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWuTD6vcdWOnWgm1e_IBCh3JGQuY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zI4_8_UDgLz4xQJ3inRMXq9XPYtwq_N4o9ybeNtkUwI',
+      useTemplateId: 'QUbJ2m28pLZuHFBOcmSiV9EtYWE_lubLJ-6JEOVdDmA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-28',
       festivals: [
